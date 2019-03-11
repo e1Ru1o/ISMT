afd42ddb1b0ac2ef251d7fb2c32fda071443822f
TNO <m.tenorio@estudiantes.matcom.uh.cu>
2019-03-10 20:38:30 -0500

    Some modifications in LoginViewModel.
    Changes:
    -EmailAddress annotation added to Email property.
    -bool RememberMe property added.

--- BizLogic/Authentication/LoginViewModel.cs
+++ BizLogic/Authentication/LoginViewModel.cs
@@ -7,11 +7,13 @@ namespace BizLogic.Authentication
 {
     public class LoginViewModel
     {
-        [Required, StringLength(100)]
+        [Required, StringLength(100), EmailAddress]
         public string Email { get; set; }
 
         [Required, StringLength(100)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
     }
 }
