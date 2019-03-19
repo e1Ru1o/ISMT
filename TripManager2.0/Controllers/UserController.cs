1a6c29c8fb8201dc98caf1a3ec223fe486dccd4d
TNO <m.tenorio@estudiantes.matcom.uh.cu>
2019-03-19 12:12:08 -0500

    Testing GetterAll results in Print View

--- TripManager2.0/Controllers/UserController.cs
+++ TripManager2.0/Controllers/UserController.cs
@@ -1,18 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BizData.Entities;
+using BizDbAccess.GenericInterfaces;
+using BizDbAccess.Utils;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Reflection;
 using TripManager2._0.ViewModels;
 
 namespace TripManager2._0.Controllers
 {
     public class UserController : Controller
     {
+        private readonly IUnitOfWork _context;
+        private readonly SignInManager<Usuario> _signInManager;
+        private readonly UserManager<Usuario> _userManager;
+
+        public UserController(IUnitOfWork context,
+            SignInManager<Usuario> signInManager,
+            UserManager<Usuario> userManager)
+        {
+            _context = context;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
         public IActionResult Welcome(TableUserViewModel user)
         {
+            user.per = PermisoTipo.admin;
             return View(user);
         }
 
         public IActionResult Print(TableUserViewModel text)
         {
-            return Content(text.Table.ToString());
+            //falta completar el diccionario
+            var getter = new GetterAll(new Dictionary<string, string> { { "Usuario", "UserDbAccess" } },
+                new AssemblyName("BizData, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"),
+                _context, _signInManager, _userManager);
+            var result1 = getter.GetAll(text.Table.ToString());
+
+            return View($"Display{text.Table.ToString()}", result1);
         }
     }
 }
