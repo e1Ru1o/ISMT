244bac7ef5b4192acac82bcc536fce81b293fc2c
TNO <m.tenorio@estudiantes.matcom.uh.cu>
2019-05-05 12:10:13 -0500

    [stdevAntiD2ta] Add new tables.
    New:
    -> Paises_Visas<Pais_Visa>
    -> Pasaportes_Visas<Pasaportes_Visas>
    -> Usuarios_Responsabilidades<Usuario_Responsabilidad>
    -> Visas<Visa>

--- DataLayer/EfCode/EfCoreContext.cs
+++ DataLayer/EfCode/EfCoreContext.cs
@@ -19,11 +19,19 @@ namespace DataLayer.EfCode
         public DbSet<Institucion> Instituciones { get; set; }
         public DbSet<Ciudad> Ciudades { get; set; }
         public DbSet<Pais> Paises { get; set; }
+        public DbSet<Pais_Visa> Paises_Visas { get; set; }
         public DbSet<Pasaporte> Pasaportes { get; set; }
+        public DbSet<Pasaporte_Visa> Pasaportes_Visas { get; set; }
         public DbSet<Responsabilidad> Responsabilidades { get; set; }
         public DbSet<EstadoViaje> EstadosViaje { get; set; }
-        public DbSet<Workflow> Workflow { get; set; }
+        public DbSet<Usuario_Responsabilidad> Usuarios_Responsabilidades { get; set; }
         public DbSet<Visa> Visas { get; set; }
+        public DbSet<Workflow> Workflow { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
 
         public int Commit()
         {
