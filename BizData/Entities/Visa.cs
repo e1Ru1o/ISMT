a8915f4228d75529b93fac78bd91d96c6f913422
TNO <m.tenorio@estudiantes.matcom.uh.cu>
2019-05-14 19:35:45 -0500

    [stdevAntiD2ta] Added a Region_Visa relationship.

--- BizData/Entities/Visa.cs
+++ BizData/Entities/Visa.cs
@@ -10,5 +10,6 @@ namespace BizData.Entities
         public string Name { get; set; }
 
         public virtual ICollection<Pais_Visa> Paises { get; set; }
+        public virtual ICollection<Region_Visa> Regiones { get; set; }
     }
 }
