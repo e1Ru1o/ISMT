e2d87157f804a2b73cf8c722e5c7f50d368b833a
Juan David Menendez del Cueto <juanda.mdc97@gmail.com>
2019-05-15 17:44:55 -0400

    add entity Historial and Historial repository

---
+++ BizData/Entities/Historial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BizData.Entities
+{
+    public class Historial
+    {
+        public int HistorialID { get; set; }
+        public Estado Estado { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public virtual Usuario Usuario { get; set; }
+        public virtual Itinerario Itinerario { get; set; }
+    }
+}
---
+++ BizDbAccess/Repositories/HistorialDbAccess.cs
@@ -0,0 +1,40 @@
+﻿using BizData.Entities;
+using BizDbAccess.GenericInterfaces;
+using DataLayer.EfCode;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BizDbAccess.Repositories
+{
+    public class HistorialDbAccess : IEntityDbAccess<Historial>
+    {
+        private readonly EfCoreContext _context;
+
+        public HistorialDbAccess(IUnitOfWork context)
+        {
+            _context = (EfCoreContext)context;
+        }
+
+        public void Add(Historial entity)
+        {
+            _context.Historial.Add(entity);
+        }
+
+        public void Delete(Historial entity)
+        {
+
+        }
+
+        public IEnumerable<Historial> GetAll()
+        {
+            return _context.Historial.OrderBy(item => item.Fecha);
+        }
+
+        public Historial Update(Historial entity, Historial toUpd)
+        {
+            return null;
+        }
+    }
+}
