dd47c4fd233795df12c3a5ca66b5cd5d71e76ca1
TNO <m.tenorio@estudiantes.matcom.uh.cu>
2019-05-23 20:12:39 -0500

    Add ViajeInvitado, services for it left.

---
+++ BizData/Entities/ViajeInvitado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BizData.Entities
+{
+    public class ViajeInvitado
+    {
+        public int ViajeInvitadoID { get; set; }
+        public DateTime? FechaLLegada { get; set; }
+        public string Procedencia { get; set; }
+        public string Nombre { get; set; }
+        public string Motivo { get; set; }
+
+        public virtual Usuario Usuario { get; set; }
+
+    }
+}
