773efbb0e63da037fe701bc2f2515b2e34ba6f01
TNO <m.tenorio@estudiantes.matcom.uh.cu>
2019-05-23 20:36:46 -0500

    Add new migrations.

---
+++ DataLayer/Migrations/20190524013444_FinalSprint-2.8.0.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataLayer.Migrations
+{
+    public partial class FinalSprint280 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ViajesInvitados",
+                columns: table => new
+                {
+                    ViajeInvitadoID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FechaLLegada = table.Column<DateTime>(nullable: true),
+                    Procedencia = table.Column<string>(nullable: true),
+                    Nombre = table.Column<string>(nullable: true),
+                    Motivo = table.Column<string>(nullable: true),
+                    UsuarioId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ViajesInvitados", x => x.ViajeInvitadoID);
+                    table.ForeignKey(
+                        name: "FK_ViajesInvitados_AspNetUsers_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ViajesInvitados_UsuarioId",
+                table: "ViajesInvitados",
+                column: "UsuarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ViajesInvitados");
+        }
+    }
+}
