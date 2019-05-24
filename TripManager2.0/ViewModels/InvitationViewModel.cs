ce6001148d15534fd3cc6364ee0888804edfede4
TNO <m.tenorio@estudiantes.matcom.uh.cu>
2019-05-23 22:26:29 -0500

    View of create viajeInvitado now gets one date only.

--- TripManager2.0/ViewModels/InvitationViewModel.cs
+++ TripManager2.0/ViewModels/InvitationViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,14 +10,13 @@ namespace TripManager2._0.ViewModels
     public class InvitationViewModel
     {
         [Required]
+        [DisplayName("Nombre y apellidos del invitado")]
         public string Name { get; set; }
 
         [Required]
         public string Procedencia { get; set; }
 
-        [Required]
-        public DateTime Start { get; set; }
-
+        [DisplayName("Fecha de llegada")]
         [Required]
         public DateTime End { get; set; }
 
