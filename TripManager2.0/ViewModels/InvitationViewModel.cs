51e7635b6ef14cac811c0709af80a6dec5d07d9e
RUL0 <riglesiasvera@gmail.com>
2019-05-24 08:15:21 -0400

    Adding InvitationTrips to ViewTrips asnd AuthorizeTrips

--- TripManager2.0/ViewModels/InvitationViewModel.cs
+++ TripManager2.0/ViewModels/InvitationViewModel.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using BizData.Entities;
 
 namespace TripManager2._0.ViewModels
 {
     public class InvitationViewModel
     {
+        public InvitationViewModel() { }
+
+        public InvitationViewModel(ViajeInvitado v)
+        {
+            Name = v.Nombre;
+            Procedencia = v.Procedencia;
+            End = v.FechaLLegada.Value;
+            //TODO: [JUANDA] Check the property name of Estado in ViajeInvitado
+            Status = v.Estado.ToString();
+            ID = v.ViajeInvitadoID;
+        }
+
         [Required]
         public string Name { get; set; }
 
@@ -15,11 +28,12 @@ namespace TripManager2._0.ViewModels
         public string Procedencia { get; set; }
 
         [Required]
-        public DateTime Start { get; set; }
-
-        [Required]
         public DateTime End { get; set; }
 
         public string Motivo { get; set; }
+
+        public string Status { get; set; }
+
+        public int ID { get; private set; }
     }
 }
